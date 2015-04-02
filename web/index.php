fcbacee9c73cd4fedf123e4fb6248315ecea2f9b
TMNorris80 <tnorris2@wisc.edu>
2015-04-02 16:30:39 -0500

    Changed return value in index.php

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return str_repeat('Hello', getenv('TIMES'));
+  return 'Hello';
 });
 
 $app->run();
