65a905fb4ba68fe656161de181d842bd6bdc5b8f
TMNorris80 <tnorris2@wisc.edu>
2015-04-03 09:17:16 -0500

    removed includes

--- web/index.php
+++ web/index.php
@@ -2,9 +2,6 @@
 
 //require('../vendor/autoload.php');
 include('../splash.html');
-include('../css/test.css');
-include('../css/foudation.css');
-include('../css/splash.css');
 //$app = fopen("../splash.html", "r") or die("Unable to open page");
 //$app['debug'] = true;
 
