1c0e45bdf1725b8e240d74eba5a5a9bb0b2009c8
jcskillings <jcskillings@gmail.com>
2015-04-03 16:22:04 +0000

    blah

--- web/index.php
+++ web/index.php
@@ -1,7 +1,11 @@
 <?php
-
+include_once("splash.html"); 
 //require('../vendor/autoload.php');
 require('../splash.html');
+set_include_path('paidit/');
+//set_include_path('classes/');
+//set_include_path('classes/');
+
 
 //$app = fopen("../splash.html", "r") or die("Unable to open page");
 //$app['debug'] = true;
