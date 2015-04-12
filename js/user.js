2c6073ab8f53871eb0e85d839780260446231f4a
kyleschenk1 <hanz84_182@yahoo.com>
2015-04-12 22:07:12 +0000

    major fixes, linking items together

---
+++ js/user.js
@@ -0,0 +1,9 @@
+function User(userName, email, firstName, lastName, phone,  password){
+        this.userName = userName;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.password = password;
+    
+}
